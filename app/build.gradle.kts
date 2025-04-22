3f4124fd0aa8cfe07c79b9512f8a5b2ca35ad96e
tobi-deruiter <zachderuiter@gmail.com>
2025-04-22 16:26:32 -0400

    added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,9 @@ android {
 
 dependencies {
 
+    implementation(libs.json)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
