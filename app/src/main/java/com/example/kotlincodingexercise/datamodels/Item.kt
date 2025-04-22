49cebb7e4b13d3d2b837cf6712f4884930c3c640
tobi-deruiter <zachderuiter@gmail.com>
2025-04-22 19:31:44 -0400

    made Item class serializable to send data to items activity

--- app/src/main/java/com/example/kotlincodingexercise/datamodels/Item.kt
+++ app/src/main/java/com/example/kotlincodingexercise/datamodels/Item.kt
@@ -1,7 +1,9 @@
 package com.example.kotlincodingexercise.datamodels
 
-data class Item (
+import java.io.Serializable
+
+data class Item(
     val id: Int,
     val listId: Int,
     val name: String
-)
+) : Serializable
