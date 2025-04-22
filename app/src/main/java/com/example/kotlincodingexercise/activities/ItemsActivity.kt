ea50dc016c0262c72b0f66548d4f5affdb55ad82
tobi-deruiter <zachderuiter@gmail.com>
2025-04-22 16:29:17 -0400

    created initial items activity for displaying sorted items

---
+++ app/src/main/java/com/example/kotlincodingexercise/activities/ItemsActivity.kt
@@ -0,0 +1,21 @@
+package com.example.kotlincodingexercise.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.kotlincodingexercise.R
+
+class ItemsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_items)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
