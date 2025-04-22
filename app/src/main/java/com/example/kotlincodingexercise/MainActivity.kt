72b8565389d63d05171a5af45f8b7b20634d2ede
tobi-deruiter <zachderuiter@gmail.com>
2025-04-22 18:18:21 -0400

    implemented getting/displaying data

--- app/src/main/java/com/example/kotlincodingexercise/MainActivity.kt
+++ app/src/main/java/com/example/kotlincodingexercise/MainActivity.kt
@@ -3,25 +3,19 @@ package com.example.kotlincodingexercise
 import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.kotlincodingexercise.api.RetrofitAPI
 import com.example.kotlincodingexercise.datamodels.Item
+import com.example.kotlincodingexercise.tools.DataManager
 import com.example.kotlincodingexercise.tools.MainAdapter
-import com.google.gson.JsonArray
-import org.json.JSONObject
-import retrofit.Call
-import retrofit.Callback
-import retrofit.GsonConverterFactory
-import retrofit.Response
-import retrofit.Retrofit
 
 class MainActivity : AppCompatActivity() {
+    val dataManager = DataManager(this@MainActivity)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -31,7 +25,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.recyclerview)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainRecyclerview)) { v, insets ->
             val innerPadding = insets.getInsets(
                 WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
             )
@@ -43,48 +37,14 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        getData()
+        dataManager.getData(::addAdapter)
     }
 
-    private fun getData() {
-        var data: JsonArray = JsonArray()
+    private fun addAdapter() {
+        findViewById<ProgressBar>(R.id.mainProgressBar).visibility = View.GONE
+        val adapter = MainAdapter(this@MainActivity, dataManager)
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://fetch-hiring.s3.amazonaws.com")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val retrofitAPI = retrofit.create(RetrofitAPI::class.java)
-        val call: Call<JsonArray>? = retrofitAPI.getData()
-
-        call!!.enqueue(object : Callback<JsonArray> {
-            override fun onResponse(response: Response<JsonArray>?, retrofit: Retrofit?) {
-                if (response?.isSuccess == true) {
-                    findViewById<ProgressBar>(R.id.progressBar).visibility = View.GONE
-                    data = response.body()
-                    addAdapter(data)
-                }
-            }
-
-            override fun onFailure(t: Throwable?) {
-                findViewById<ProgressBar>(R.id.progressBar).visibility = View.GONE
-                Toast.makeText(this@MainActivity, "Failed to retrieve json data...", Toast.LENGTH_LONG)
-                    .show()
-            }
-        })
-    }
-
-    private fun addAdapter(jsonData: JsonArray) {
-        val data = ArrayList<Item>()
-        jsonData.let {
-            for (i in 0 until it.size()) {
-                val item = JSONObject(it.get(i).toString())
-                data.add(Item(item.getInt("id"), item.getInt("listId"), item.getString("name")))
-            }
-        }
-        val adapter = MainAdapter(data)
-
-        val recyclerview: RecyclerView = findViewById(R.id.recyclerview)
+        val recyclerview: RecyclerView = findViewById(R.id.mainRecyclerview)
         recyclerview.layoutManager = GridLayoutManager(this, 2)
         recyclerview.adapter = adapter
     }
--- app/src/main/java/com/example/kotlincodingexercise/tools/MainAdapter.kt
+++ app/src/main/java/com/example/kotlincodingexercise/tools/MainAdapter.kt
@@ -1,14 +1,18 @@
 package com.example.kotlincodingexercise.tools
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlincodingexercise.R
+import com.example.kotlincodingexercise.activities.ItemsActivity
 import com.example.kotlincodingexercise.datamodels.Item
 
-class MainAdapter(private val list: List<Item>) : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+class MainAdapter(private val context: Context, private val dataManager: DataManager)
+        : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val listIdView: TextView = itemView.findViewById(R.id.card_listid)
     }
@@ -19,11 +23,18 @@ class MainAdapter(private val list: List<Item>) : RecyclerView.Adapter<MainAdapt
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = list[position]
-        holder.listIdView.text = item.listId.toString()
+        val listId = dataManager.data.keys.toList()[position]
+        holder.listIdView.text = listId.toString()
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, ItemsActivity::class.java)
+            intent.putExtra("ListId", listId)
+            intent.putExtra("dataManager", dataManager)
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return dataManager.data.keys.size
     }
 }
