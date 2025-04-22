6823793bfa84bc2d7692e5bfe48c024c9acf8cc0
tobi-deruiter <zachderuiter@gmail.com>
2025-04-22 18:17:25 -0400

    increased minSDK to 33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.kotlincodingexercise"
-        minSdk = 24
+        minSdk = 33
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
